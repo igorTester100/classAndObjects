2ea0c1bbd1c7779e749bdc5727625b09b61a9784
Igor Grabowski <aigor2789@gmail.com>
2022-08-30 15:12:51 +0200

    Solution of homework

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -6,12 +6,40 @@ public class Bug {
     // - priorytet bledu (1-5)
     // - status bledu (otwarty/ zamkniety) - domyslnie, podczas tworzenia zawsze otwarty
 
+    private String bugDescription;
+    private String  email;
+    private int bugPriority;
+    private boolean bugStatus;
+
     // KONSTRUKTOR
+
+    Bug(String bugDescription, String email, int bugPriority) {
+        this.bugDescription = bugDescription;
+        this.email = email;
+        this.bugPriority = bugPriority;
+        this.bugStatus = false;
+    }
 
     // METODY
     // - pokaz wszystkie informacje o bledzie
     // - pokaz osobe zglaszajace blad
     // - pokaz status bledu
     // - zwroc priorytet bledu
+
+    public void getAllInfo() {
+        System.out.println(bugDescription + " reporte by " + email + " bugPriority is " + bugPriority + bugStatus);
+    }
+
+    public void getReporter() {
+        System.out.println("Reproter is " + email);
+    }
+
+    public int getBugPriority() {
+        return bugPriority;
+    }
+
+    public boolean bugStatus() {
+        return bugStatus;
+    }
 
 }
--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -6,6 +6,13 @@ public class MainApp {
         bartek.getAllInfo();
 
 
+        Bug bug1 = new Bug("Some small issue", "tester@gmail.com", 1);
+
+        bug1.getAllInfo();
+        bug1.getReporter();
+        System.out.println(bug1.getBugPriority());
+
+
     }
 
 
