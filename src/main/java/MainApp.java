bd89d0a9eb0b75251e375ef3d5e4f657a005106b
Igor Grabowski <aigor2789@gmail.com>
2022-08-31 11:26:53 +0200

    Remove uncessary code from MainApp

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,16 +1,8 @@
-import utils.StringUtils;
-import utils.WeekUtils;
-
 public class MainApp {
 
     public static void main(String[] args) {
 
-        String bartekTest = StringUtils.getFormattedText("BartekTest");
 
-        System.out.println(bartekTest);
-
-        System.out.println(WeekUtils.MONDAY);
-        System.out.println(WeekUtils.TUESDAY);
 
     }
 
