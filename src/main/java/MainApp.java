ccded1e84dbf074af7b2a681fbbfedf2ec7617cd
Igor Grabowski <aigor2789@gmail.com>
2022-09-05 13:37:58 +0200

    Training inheritance tasks

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,9 +1,19 @@
+import model.computer.Laptop;
+import model.computer.PC;
+
 public class MainApp {
 
     public static void main(String[] args) {
 
+        //1. IS A - jest czyms - DZIEDZICZENIE
+        //2. HAS A - posiada cos - KOMPOZYCJA
 
+        PC officeComputer = new PC("Office Computer", "HP",500, 120);
+        Laptop gamingLaptop = new Laptop("XGames", "HP Games", 500, 256,50);
 
+        officeComputer.switchOn();
+        gamingLaptop.switchOn();
+        officeComputer.switchOn();
     }
 
 
---
+++ src/main/java/model/computer/PC.java
@@ -0,0 +1,29 @@
+package model.computer;
+
+public class PC extends Computer{
+
+    private boolean isPowerSupply;
+
+    public PC(String name, String type, int hdd, int ram) {
+        super(name, type, hdd, ram);
+        isPowerSupply = false;
+    }
+
+    public void setPowerSupply(boolean powerSupply) {
+        isPowerSupply = powerSupply;
+    }
+
+    public void showComputerName() {
+        System.out.println(name);
+    }
+
+    @Override
+    public void switchOn() {
+        System.out.println("Checking power supply.");
+        if(isPowerSupply == true) {
+            super.switchOn();
+        } else {
+            System.out.println("Power supply missing!");
+        }
+    }
+}
